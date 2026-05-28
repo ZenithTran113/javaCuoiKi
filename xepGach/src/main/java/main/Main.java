f724faade2bb771d45fa4e51029e2a738e5c5052
ZenithTran113 <gohanlvt123@gmail.com>
2026-05-28 10:36:02 +0700

    Update Main.java

--- xepGach/src/main/java/main/Main.java
+++ xepGach/src/main/java/main/Main.java
@@ -7,16 +7,13 @@ import controller.TetrisController;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Khởi tạo các thành phần cơ bản
         JFrame frame = new JFrame("Tetris Online Đối Kháng");
         TetrisModel model = new TetrisModel();
         TetrisView view = new TetrisView(model);
         TetrisController controller = new TetrisController(model, view);
 
-        // 2. KẾT NỐI VIEW VỚI CONTROLLER
         view.setController(controller);
 
-        // 3. Thiết lập Frame
         frame.add(view);
         frame.addKeyListener(controller);
         frame.pack();
@@ -25,7 +22,6 @@ public class Main {
         frame.setResizable(false);
         frame.setVisible(true);
 
-        // 4. Lựa chọn chế độ chơi
         String[] options = {"Làm Chủ Phòng (Server)", "Vào Phòng (Client)", "Chơi Offline"};
         int choice = JOptionPane.showOptionDialog(frame,
                 "Chào mừng bạn đến với Tetris!\nChọn chế độ chơi:",
@@ -34,25 +30,21 @@ public class Main {
                 JOptionPane.QUESTION_MESSAGE,
                 null, options, options[0]);
 
-        // 5. Xử lý logic khởi chạy
-        if (choice == 0) { // Server
+        if (choice == 0) { 
             System.out.println("Đang mở cổng 8888, chờ đối thủ...");
-            // Khi kết nối thành công, SocketManager sẽ gọi controller.startGame(true)
             controller.getSocketManager().startServer(8888, controller);
         }
-        else if (choice == 1) { // Client
+        else if (choice == 1) { 
             String ip = JOptionPane.showInputDialog(frame, "Nhập IP máy chủ (ví dụ: 192.168.1.5):", "localhost");
             if (ip != null && !ip.isEmpty()) {
-                //Khi kết nối thành công, SocketManager sẽ gọi controller.startGame(true)
                 controller.getSocketManager().connect(ip, 8888, controller);
             } else {
                 System.out.println("Hủy kết nối.");
                 System.exit(0   );
             }
         }
-        else if (choice == 2) { // Chơi Offline
+        else if (choice == 2) {
             JOptionPane.showMessageDialog(frame, "Chế độ Offline: Nhấn OK để bắt đầu tính giờ!");
-            // CẬP NHẬT: Truyền 'false' vào startGame để báo hiệu chơi Offline
             controller.startGame(false);
         }
         else {
